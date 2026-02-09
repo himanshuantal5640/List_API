d76bd71224fc2ce27469003f95feb62ffb8faf6e
himanshu-antal <himanshuantal26@gmail.com>
2026-02-09 11:41:03 +0530

    initiazation

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
 const express = require('express');
 const app = express();
 const dotenv = require('dotenv');
+const connectDB = require('./config/DB.js');
+
+dotenv.config();
+connectDB();
+
+app.use(express.json());
+app.get('/',(req,res)=>{
+    res.send("User Management API");
+});
+
+module.exports = app
